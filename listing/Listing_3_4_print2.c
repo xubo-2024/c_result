a30c37ea9a9e60b9e38ea41ec0668e9aefd78726
xubo-2024 <xb1231472580@163.com>
2026-03-13 15:32:01 +0800

    fix: 修复printf语句中的格式错误并添加输出示例

--- listing/Listing_3_4_print2.c
+++ listing/Listing_3_4_print2.c
@@ -8,10 +8,17 @@ int main(void)
     long long verybig = 12345678908642;
 
     printf("un = %u and not %d\n",un,un);
-    printf("end = %hd and %d\n,",end,end);
+    printf("end = %hd and %d\n",end,end);
     printf("big = %ld and not %hd\n",big,big);
     printf("verybig = %lld and not %ld\n",verybig,verybig);
 
     return 0;
 
 }
+/*
+Here is the output on one system(result can vary):
+un = 3000000000 and not -1294967296
+end = 200 and 200
+big = 65537 and not 1
+verybig = 12345678908642 and not 1942899938
+*/
