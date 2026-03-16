1dadf948a785be3f2301524a9c0c37d161f19a55
xubo-2024 <xb1231472580@163.com>
2026-03-16 18:47:58 +0800

    Add 3_3_alert C program
    Add C program for exercise 3.3: emits an alert (\007) and prints the two lines: Startled by the sudden sound, Sally shouted, and "By the Great Pumpkin, what was that!". Includes stdio.h, defines main(), prints the messages, and returns 0.

--- Programming_Exercises/3_3_alert.c
+++ Programming_Exercises/3_3_alert.c
@@ -3,3 +3,12 @@ Write a program that sounds an alert and then prints the following text:
 Startled by the sudden sound, Sally shouted,
 "By the Great Pumpkin, what was that!"
 */
+#include<stdio.h>
+int main()
+{
+    printf("\007\n");
+    printf("Startled by the sudden sound, Sally shouted,\n");
+    printf("\"By the Great Pumpkin, what was that!\"");
+
+    return 0;
+}
